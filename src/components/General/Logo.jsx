9b2a01d43c8d9f0bc34f0ae16d1c8367dbddc473
Leksa <admin@petiteweb.ru>
2023-04-15 11:28:04 +0300

    Навели красоту в footer

--- src/components/General/Logo.jsx
+++ src/components/General/Logo.jsx
@@ -1,6 +1,6 @@
 import logoImg from "../../assents/images/logo.png";
 
-const Logo = () => <a className="logo">
+const Logo = () => <a className="logo" href="/">
     {/* <span className="logo__pic"></span> */}
     <img src={logoImg} alt="DogFood" />
     <span className="logo__text">DogFood</span>
